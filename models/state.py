8d548df914a7149eb9ebc1c0b2d4501938167c59
G_RIMA <gikandimaria178@gmail.com>
2022-09-14 19:36:50 +0300

    States and Cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqqlalchemy,orm import relationship
+from models.city import City
+import models
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __table__ = 'states'
+    name = Column(string(128), nullable=False)
+    cities = relationship("City", backref='state', cacade="all, delete, delete-orphan")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ returns the list of citie"""
+            list_city = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
+        
